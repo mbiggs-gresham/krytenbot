e82260c3c1e36d1837ca4d3c00fade1e0630daf9
Matt Biggs <mbiggs@greshamtech.com>
2024-08-21 13:59:56 +0100

    Improve tests.

--- __tests__/fixtures/mutations/updatepullrequestbranch.ts
+++ __tests__/fixtures/mutations/updatepullrequestbranch.ts
@@ -1,7 +1,10 @@
+import fetchMock from 'fetch-mock'
+import MockOptionsMethodPost = fetchMock.MockOptionsMethodPost
 import { updatePullRequestBranchMutation as mutation } from '../../../src/github-helper-queries'
 
-export const updatePullRequestBranchMutation = (): object => ({
+export const updatePullRequestBranchMutation = (options?: MockOptionsMethodPost): MockOptionsMethodPost => ({
   body: { query: mutation() },
   matchPartialBody: true,
-  overwriteRoutes: false
+  overwriteRoutes: false,
+  ...options
 })
--- __tests__/fixtures/queries/finddraftrelease.ts
+++ __tests__/fixtures/queries/finddraftrelease.ts
@@ -1,7 +1,10 @@
+import fetchMock from 'fetch-mock'
+import MockOptionsMethodPost = fetchMock.MockOptionsMethodPost
 import { findDraftReleaseQuery as query } from '../../../src/github-helper-queries'
 
-export const findDraftReleaseQuery = (): object => ({
+export const findDraftReleaseQuery = (options?: MockOptionsMethodPost): MockOptionsMethodPost => ({
   body: { query: query() },
   matchPartialBody: true,
-  overwriteRoutes: false
+  overwriteRoutes: false,
+  ...options
 })
